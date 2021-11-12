4dfe7861f11f8ee2d02b6efd10e066f1da00c2d6
Mizuna <bizbear.mirai@gmail.com>
2021-11-12 17:19:13 +0900

    gitignore check

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+const myLiffId = process.env.MY_LIFF_ID;
+//const bodyParser = require('body-parser');
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+app.use("/", require("./routes/index"))
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
