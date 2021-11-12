eaff7e61997603f70ba78880b1725c716c0244fa
Mizuna <bizbear.mirai@gmail.com>
2021-11-12 17:18:00 +0900

    gitignore cache rm

--- db/db.js
+++
@@ -1,7 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-exports.pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-});
