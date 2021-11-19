cb8dfdf86c1ecbecde387dec62ab4155d0a33888
Mizuna <bizbear.mirai@gmail.com>
2021-11-19 17:17:18 +0900

    若干ゃ修正

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -14,17 +14,17 @@
 // });
 
 // ローカル動作確認のときコメントアウト
-// console.log("load!!")
-// liff.init({
-//     liffId: '1656528096-W6qpn7m3',
-//     // withLoginOnExternalBrowser: true,
-// }).then(() => {
-//     console.log("liff inited")
-//     if (!liff.isLoggedIn()) {
-//         liff.login();
-//     }
-//     const idToken = liff.getDecodedIDToken();
-//     console.log(idToken);
-// }).catch((err) => {
-//     console.log(err);
-// });
+console.log("load!!")
+liff.init({
+    liffId: '1656528096-W6qpn7m3',
+    // withLoginOnExternalBrowser: true,
+}).then(() => {
+    console.log("liff inited")
+    if (!liff.isLoggedIn()) {
+        liff.login();
+    }
+    const idToken = liff.getDecodedIDToken();
+    console.log(idToken);
+}).catch((err) => {
+    console.log(err);
+});
