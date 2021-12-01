a80401e880cff61ff24835207e68bfd3bbd3f1c8
KentaroJay <kentaro.jay.takahashi@gmail.com>
2021-12-01 13:12:55 +0900

    hot fix.

--- public/post.js
+++ public/post.js
@@ -6,16 +6,19 @@ liff.init({ //いろいろ初期化
 }).then(() => { //初期化完了後にこれを実行
     console.log("liff inited")
     if (!liff.isLoggedIn()) {
+        console.log("not logged in")
         liff.login(); //ログインしていなかったらログイン
-    }
-    const line_profile = await liff.getProfile() //プロフィール取得
-    const line_id = line_profile.userId //lineIDの取得
-    let element = document.getElementById('line_id');
-    element.value = line_id;
+    } else {
+        console.log("logged in")
+        const line_profile = await liff.getProfile() //プロフィール取得
+        const line_id = line_profile.userId //lineIDの取得
+        let element = document.getElementById('line_id');
+        element.value = line_id;
 
-    // 取得できてるか確認
-    console.log(line_id);
-    console.log(document.getElementById('line_id').value);
+        // 取得できてるか確認
+        console.log(line_id);
+        console.log(document.getElementById('line_id').value);
+    }
 }).catch((err) => { //エラー処理
     console.log(err);
 });
