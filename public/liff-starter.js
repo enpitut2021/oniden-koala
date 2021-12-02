0294c2c1880298d49fe8ce10d4fdb5cada2abe20
KentaroJay <kentaro.jay.takahashi@gmail.com>
2021-12-02 16:17:50 +0900

    fixed post button on index.ejs, changed notification message on lineout-screen. and other minor changes.

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -4,12 +4,12 @@ liff.init({ //いろいろ初期化
     liffId: '1656528096-W6qpn7m3',
     // withLoginOnExternalBrowser: true,
 }).then(() => { //初期化完了後にこれを実行
-    console.log("liff inited")
-    if (!liff.isLoggedIn()) {
-        liff.login(); //ログインしていなかったらログイン
-    }
-    const idToken = liff.getDecodedIDToken(); //ユーザーの情報を取得
-    console.log(idToken);
+    // console.log("liff inited")
+    // if (!liff.isLoggedIn()) {
+    //     liff.login(); //ログインしていなかったらログイン
+    // }
+    // const idToken = liff.getDecodedIDToken(); //ユーザーの情報を取得
+    // console.log(idToken);
 }).catch((err) => { //エラー処理
     console.log(err);
 });
