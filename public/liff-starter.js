5ccc04b1f6e44eb055568fd94f1b802a08a2f357
Mizuna <bizbear.mirai@gmail.com>
2021-12-02 16:11:28 +0900

    liff-starter.js コメントアウト解除

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,15 +1,15 @@
-// // ローカル動作確認のときコメントアウト
-// console.log("load!!")
-// liff.init({ //いろいろ初期化
-//     liffId: '1656528096-W6qpn7m3',
-//     // withLoginOnExternalBrowser: true,
-// }).then(() => { //初期化完了後にこれを実行
-//     console.log("liff inited")
-//     if (!liff.isLoggedIn()) {
-//         liff.login(); //ログインしていなかったらログイン
-//     }
-//     const idToken = liff.getDecodedIDToken(); //ユーザーの情報を取得
-//     console.log(idToken);
-// }).catch((err) => { //エラー処理
-//     console.log(err);
-// });
+// ローカル動作確認のときコメントアウト
+console.log("load!!")
+liff.init({ //いろいろ初期化
+    liffId: '1656528096-W6qpn7m3',
+    // withLoginOnExternalBrowser: true,
+}).then(() => { //初期化完了後にこれを実行
+    console.log("liff inited")
+    if (!liff.isLoggedIn()) {
+        liff.login(); //ログインしていなかったらログイン
+    }
+    const idToken = liff.getDecodedIDToken(); //ユーザーの情報を取得
+    console.log(idToken);
+}).catch((err) => { //エラー処理
+    console.log(err);
+});
