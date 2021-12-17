cd3e9a15587112fc4ca6314ce5944ed646580cde
ninjamagicston <s1911494@klis.tsukuba.ac.jp>
2021-12-17 16:37:43 +0900

    16:37

--- public/top.js
+++ public/top.js
@@ -3,7 +3,7 @@ console.log("load!!")
 liff.init({ //いろいろ初期化
     liffId: '1656528096-W6qpn7m3',
     // withLoginOnExternalBrowser: true,
-}).then(async () => { //初期化完了後にこれを実行
+}).then(async() => { //初期化完了後にこれを実行
     console.log("liff inited")
     if (!liff.isLoggedIn()) {
         console.log("not logged in")
@@ -14,6 +14,20 @@ liff.init({ //いろいろ初期化
         const line_id = line_profile.userId //lineIDの取得
         let element = document.getElementById('line_id');
         element.value = line_id;
+
+        // ticketsapi叩く
+        let request = new XMLHttpRequest();
+        request.open('GET', '/get-tickets', true);
+        request.responseType = 'json';
+        request.onload = function() {
+            // レスポンスが返ってきた時の処理
+            const tickets = this.response[0].tickets;
+            console.log('api response is ...')
+            console.log(this.response)
+        }
+
+        // リクエストをURLに送信
+        request.send();
 
         // 取得できてるか確認
         console.log(line_id);
